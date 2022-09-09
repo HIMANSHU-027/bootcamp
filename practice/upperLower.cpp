f951ce7d0b2dba9017558704979c9a5b536c62ef
HIMANSHU <himu2751@gmail.com>
2022-09-10 00:18:17 +0530

    upper to lower

--- practice/upperLower.cpp
+++ practice/upperLower.cpp
@@ -4,8 +4,8 @@ int main(){
     char a[10] = "HuMan"; 
 
    for(int i=0; i<5; i++){
-    if(int(a[i])>= 97 && int(a[i]) <= 122)
-      a[i] = int(a[i])-32;
+    if(int(a[i])>= 65 && int(a[i]) <= 90)
+      a[i] = int(a[i])+32;
     cout << a[i];
    } 
     return 0; 
